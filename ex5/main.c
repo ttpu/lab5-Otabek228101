a17a4dff2ea0c3c55e8f34d78e914a463fa11d97
Otabek228101 <bekotabekbek56@gmail.com>
2024-11-06 11:11:52 +0500

    pls , Check it out

--- ex5/main.c
+++ ex5/main.c
@@ -1,7 +1,15 @@
-
 #include <stdio.h>
 
-int main(void)
-{
-    printf("Hello World\n");
+int main() {
+    char small_letter, capital_letter;
+
+
+    for (int i = 0; i < 26; i++) {
+        small_letter = 'a' + i; 
+        capital_letter = 'A' + i; 
+
+        printf("%c\t\t%d\t\t%c\t\t%d\n", small_letter, small_letter, capital_letter, capital_letter);
+    }
+
+    return 0;
 }
--- ex7/main.c
+++ ex7/main.c
@@ -1,7 +1,35 @@
-
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(void)
-{
-    printf("Hello World\n");
+int main() {
+    int n, i;
+    bool big = false, min = false;
+    
+    printf("n: ");
+    scanf("%d", &n);
+    
+    int arr[n]; 
+    
+    for (i = 0; i < n; i++) {
+        printf("number: ");
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 1; i < n; i++) {
+        if (arr[i] > arr[i - 1]) {
+            big = true;
+        } else if (arr[i] < arr[i - 1]) {
+            min = true;
+        }
+    }
+
+    if (big && !min) {
+        printf("ascending sequence\n");
+    } else if (min && !big) {
+        printf("descending sequence\n");
+    } else {
+        printf("neither ascending nor descending sequence\n");
+    }
+    
+    return 0;
 }
